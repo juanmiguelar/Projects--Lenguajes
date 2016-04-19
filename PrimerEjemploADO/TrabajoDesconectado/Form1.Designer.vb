e1171c1fb0db083f206d9d75f83b3b33e2661117
Juan Miguel <juan.ariasmejias@ucr.ac.cr>
2016-04-19 15:09:23 -0600

    Trabajo desconectado.

---
+++ PrimerEjemploADO/TrabajoDesconectado/Form1.Designer.vb
@@ -0,0 +1,76 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.grdDatos = New System.Windows.Forms.DataGridView()
+        Me.btnCargar = New System.Windows.Forms.Button()
+        Me.btnGuardar = New System.Windows.Forms.Button()
+        CType(Me.grdDatos, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'grdDatos
+        '
+        Me.grdDatos.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
+        Me.grdDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.grdDatos.Location = New System.Drawing.Point(21, 12)
+        Me.grdDatos.Name = "grdDatos"
+        Me.grdDatos.Size = New System.Drawing.Size(240, 150)
+        Me.grdDatos.TabIndex = 0
+        '
+        'btnCargar
+        '
+        Me.btnCargar.Location = New System.Drawing.Point(12, 214)
+        Me.btnCargar.Name = "btnCargar"
+        Me.btnCargar.Size = New System.Drawing.Size(75, 23)
+        Me.btnCargar.TabIndex = 1
+        Me.btnCargar.Text = "Cargar"
+        Me.btnCargar.UseVisualStyleBackColor = True
+        '
+        'btnGuardar
+        '
+        Me.btnGuardar.Location = New System.Drawing.Point(177, 214)
+        Me.btnGuardar.Name = "btnGuardar"
+        Me.btnGuardar.Size = New System.Drawing.Size(75, 23)
+        Me.btnGuardar.TabIndex = 2
+        Me.btnGuardar.Text = "Guardar"
+        Me.btnGuardar.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.btnGuardar)
+        Me.Controls.Add(Me.btnCargar)
+        Me.Controls.Add(Me.grdDatos)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.grdDatos, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents grdDatos As DataGridView
+    Friend WithEvents btnCargar As Button
+    Friend WithEvents btnGuardar As Button
+End Class
---
+++ PrimerEjemploADO/TrabajoDesconectado/Form1.vb
@@ -0,0 +1,84 @@
+﻿Imports System.Data.SqlClient
+Public Class Form1
+
+    Dim conexion As New SqlConnection(My.Settings.connectionString)
+    Dim tablaDatos As New DataTable
+    Dim adaptador As New SqlDataAdapter
+
+    Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+
+        adaptador.DeleteCommand = crearDelete()
+        adaptador.InsertCommand = crearInsert()
+        adaptador.UpdateCommand = crearUpdate()
+    End Sub
+
+    Private Sub btnCargar_Click(sender As Object, e As EventArgs) Handles btnCargar.Click
+
+        ' Vaciamos la datable
+        tablaDatos.Clear()
+
+        ' Setteamos el comando
+        adaptador.SelectCommand = New SqlCommand("SELECT * FROM Personas", conexion)
+
+        ' Llenamos la datable
+        adaptador.Fill(tablaDatos)
+
+        grdDatos.DataSource = tablaDatos
+    End Sub
+
+    Private Sub btnGuardar_Click(sender As Object, e As EventArgs) Handles btnGuardar.Click
+
+        adaptador.Update(tablaDatos)
+    End Sub
+
+    Private Function crearDelete() As SqlCommand
+        Dim retorno As New SqlCommand(
+            "DELETE Personas WHERE Codigo=@cod", conexion)
+
+        ' Es un objeto de tipo parametro que nos va a ayudar a ingresar el parametro a la sentencia
+        Dim param1 As New SqlParameter("cod", SqlDbType.Int, 4, "Codigo")
+
+        ' Le digo al comando que parametro
+        retorno.Parameters.Add(param1)
+
+        Return retorno
+    End Function
+
+    Private Function crearInsert() As SqlCommand
+        Dim retorno As New SqlCommand(
+            "INSERT INTO Personas VALUES(@cod, @nombre, @apellido)", conexion)
+
+        ' Es un objeto de tipo parametro que nos va a ayudar a ingresar el parametro a la sentencia
+        Dim paramCod As New SqlParameter("cod", SqlDbType.Int, 4, "Codigo")
+        Dim paramNombre As New SqlParameter("nombre", SqlDbType.NVarChar, 50, "Nombre")
+        Dim paramApellido As New SqlParameter("apellido", SqlDbType.NVarChar, 50, "Apellido")
+
+        ' Le digo al comando que parametro
+        retorno.Parameters.Add(paramCod)
+        retorno.Parameters.Add(paramNombre)
+        retorno.Parameters.Add(paramApellido)
+
+        Return retorno
+    End Function
+
+    Private Function crearUpdate() As SqlCommand
+        Dim retorno As New SqlCommand("UPDATE Personas SET Nombre=@nombre, Apellido=@apellido WHERE Codigo=@cod", conexion)
+
+        ' Es un objeto de tipo parametro que nos va a ayudar a ingresar el parametro a la sentencia
+        Dim paramCod As New SqlParameter("cod", SqlDbType.Int, 4, "Codigo")
+        Dim paramNombre As New SqlParameter("nombre", SqlDbType.NVarChar, 50, "Nombre")
+        Dim paramApellido As New SqlParameter("apellido", SqlDbType.NVarChar, 50, "Apellido")
+
+        ' Le digo al comando que parametro
+        retorno.Parameters.Add(paramCod)
+        retorno.Parameters.Add(paramNombre)
+        retorno.Parameters.Add(paramApellido)
+
+        Return retorno
+    End Function
+End Class
